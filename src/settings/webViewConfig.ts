924b76fa1ad70c0e2b133ff0fa01324ac3d52671
cloud-mouse <>
2024-10-04 20:32:08 +0800

    feat: change somting

--- src/settings/webViewConfig.ts
+++ src/settings/webViewConfig.ts
@@ -10,18 +10,18 @@ if (platform === 'MP-WEIXIN') {
 	environment = envVersion;
 	// * h5跳转域名
 	FORM_BASEURL = {
-		develop: 'https://gensci-isdm-test.zjwlyy.cn/h5/patient', //开发版
-		trial: 'https://gensci-isdm-test.zjwlyy.cn/h5/patient', //体验版
-		release: 'https://followup.child-healthy.cn/h5/patient' //正式版
+		develop: '', //开发版
+		trial: '', //体验版
+		release: '' //正式版
 	};
 }
 if (platform === 'H5') {
 	environment = getEnvMode();
 	// * h5跳转域名
 	FORM_BASEURL = {
-		development: 'https://gensci-isdm-test.zjwlyy.cn/h5/patient', //开发版
-		test: 'https://gensci-isdm-test.zjwlyy.cn/h5/patient', //体验版
-		production: 'https://followup.child-healthy.cn/h5/patient' //正式版
+		development: '', //开发版
+		test: '', //体验版
+		production: '' //正式版
 	};
 }
 export const PORT1 = FORM_BASEURL[environment];
